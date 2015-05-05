5de6581e594409267e2f38715edcb8f37658b38c
David Sheridan <sheriddavi@stellar.ecs.vuw.ac.nz>
2015-05-05 12:32:11 +1200

    Added new package to hold xml data. Tested parsing file via the Main class.

--- src/kps/Main.java
+++ src/kps/Main.java
@@ -1,7 +1,21 @@
 package kps;
 
+import java.io.File;
+import java.util.List;
+
+import kps.events.BusinessEvent;
+import kps.parser.KPSParser;
+import kps.parser.ParserException;
+
 public class Main {
+
+	public static final String XML_FILE_PATH = "src"+File.separatorChar+"kps"+File.separatorChar+"xml"+File.separatorChar;
+	public static final String filename = XML_FILE_PATH + "kps2.xml";
+
 	public static void main(String[] args){
-		
+		try{
+			List<BusinessEvent> eventLog = KPSParser.parseFile(filename);
+			System.out.println("file parsed successfully");
+		}catch(ParserException e){e.printStackTrace();}
 	}
 }
---
+++ src/kps/parser/tests/Test.java
@@ -0,0 +1,5 @@
+package kps.parser.tests;
+
+public class Test {
+
+}
