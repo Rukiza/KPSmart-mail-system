be5b15cea8199b1085adfddb0085399837c742ce
David Sheridan <sheriddavi@globe.ecs.vuw.ac.nz>
2015-05-06 12:21:12 +1200

    Added a method in KPSmartSystem to convert the event log into an XML file. The XML file is currently stored in the xml package. Also updated the filename that is referencing the temporary data.

--- src/kps/KPSmartSystem.java
+++ src/kps/KPSmartSystem.java
@@ -1,5 +1,8 @@
 package kps;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 import kps.data.CustomerRoute;
@@ -21,7 +24,22 @@ public class KPSmartSystem {
 	private List<KPSUser> users;
 	private KPSUser currentUser;
 
+	private final String EVENT_LOG_FILENAME = Main.XML_FILE_PATH+"kps_data.xml";
+
 	public KPSmartSystem(){
 
+	}
+
+	/**
+	 * Generates and XML file from the event log.
+	 */
+	public void convertEventLogToXML(){
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter(EVENT_LOG_FILENAME, "UTF-8");
+			writer.write(eventLog.toXML());
+			writer.close();
+		}catch(FileNotFoundException e){e.printStackTrace();}
+		catch(UnsupportedEncodingException e){e.printStackTrace();}
 	}
 }
