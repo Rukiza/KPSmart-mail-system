e7b41a0eb152958525d8c35cb14941e8dc95c12a
David Sheridan <dpmsheridan@hotmail.com>
2015-05-07 14:36:19 +1200

    Updated the javadocs for DeliveryPrice to specify that the costs are in cents.

--- src/kps/data/wrappers/DeliveryPrice.java
+++ src/kps/data/wrappers/DeliveryPrice.java
@@ -3,62 +3,62 @@ package kps.data.wrappers;
 /**
  * Represents the current delivery price for sending mail from
  * a particular origin to destination.
- * 
+ *
  * @author David
  *
  */
 public class DeliveryPrice {
-	
+
 	// fields
-	private double gramPrice; // price per gram
-	private double volumePrice; // price per cubic centimeter
-	
+	private double gramPrice; // price per gram in cents
+	private double volumePrice; // price per cubic centimeter in cents
+
 	/**
 	 * Constructs a new Delivery Price based on the specified gram and
 	 * volume prices.
-	 * 
+	 *
 	 * @param gramPrice
-	 * 		-- the price per gram
+	 * 		-- the price per gram in cents
 	 * @param volumePrice
-	 * 		-- the price per cubic centimeter
+	 * 		-- the price per cubic centimeter in cents
 	 */
 	public DeliveryPrice(double gramPrice, double volumePrice){
 		this.gramPrice = gramPrice;
 		this.volumePrice = volumePrice;
 	}
-	
+
 	/**
 	 * Returns the price per gram for this delivery price.
-	 * 
-	 * @return gram price (per gram)
+	 *
+	 * @return gram price in cents (per gram)
 	 */
 	public double getGramPrice(){
 		return gramPrice;
 	}
-	
+
 	/**
-	 * Returns the price per cubic centimeter for this delivery
+	 * Returns the price per cubic centimeter in cents for this delivery
 	 * price.
-	 * 
-	 * @return volume price (per cubic centimeter)
+	 *
+	 * @return volume price in cents (per cubic centimeter)
 	 */
 	public double getVolumePrice(){
 		return volumePrice;
 	}
-	
+
 	/**
-	 * Updates the gram and volume prices to the specified values.
-	 * 
+	 * Updates the gram and volume prices in cents to the specified values.
+	 *
 	 * @param gramPrice
-	 * 		-- the new price per gram
+	 * 		-- the new price per gram in cents
 	 * @param volumePrice
-	 * 		-- the new price per cubic centimeter
+	 * 		-- the new price per cubic centimeter in cents
 	 */
 	public void updateDeliveryPrice(double gramPrice, double volumePrice){
 		this.gramPrice = gramPrice;
 		this.volumePrice = volumePrice;
 	}
-	
+
 	public boolean equals(Object o){
 		if(o == this){
 			return true;
